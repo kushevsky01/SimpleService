1aa300ad8a2fc1ef5ad45dc99707cb2cadabbe20
kushevsky01 <78921029+kushevsky01@users.noreply.github.com>
2022-06-22 13:36:17 +0300

    Add files via upload

--- SimpleService/src/main/java/service/simpleservice/OperationConroller.java
+++ SimpleService/src/main/java/service/simpleservice/OperationConroller.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OperationConroller {
     @GetMapping("/calc")
-    public SubEntity getCalc( @RequestParam(value = "command", defaultValue = "no command") String command,@RequestParam(value = "a",defaultValue = "0") String a, @RequestParam(value = "b",defaultValue = "0") String b) {
+    public Result getCalc( @RequestParam(value = "command", defaultValue = "no command") String command,@RequestParam(value = "a",defaultValue = "0") String a, @RequestParam(value = "b",defaultValue = "0") String b) {
         int result = Calculate.calculateParameter(command, a, b);
-        return new SubEntity(command, a, b, result);
+        return new Result(command, a, b, result);
     }
 
 }
